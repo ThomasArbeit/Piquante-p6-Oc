b08a451c2d6100964ed4940f3a64f9366ee8b69f
Thomas Arbeit <tarbeit@hotmail.fr>
2020-09-02 17:06:43 +0200

    Ajout de la possibilité de gérer les fichiers images

--- routes/sauce.js
+++ routes/sauce.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
